b7f0dc26de83e269cb2fac0ddd16ae73cc1a1eb9
V1RTEX <oleg04011999@gmail.com>
2020-12-03 22:00:33 +0200

    an in-memory book repository implementation

---
+++ infastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] book = new[]
+        {
+            new Book(1, "Art of Programming"),
+            new Book(1, "Refactoring"),
+            new Book(1, "C Programming Language")
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return book.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
+    }
+}
